36247ae218bc32e3541720ccead601cdfd2beda2
Felienarius <pawel.piekarus@gmail.com>
2021-04-26 06:19:36 +0200

    styles tweaks and animations

--- src/components/MoviesList.tsx
+++ src/components/MoviesList.tsx
@@ -1,22 +1,35 @@
+import { FunctionComponent } from "react";
 import Movie from "./Movie";
 import { MovieData } from "./Movie";
 import classes from "./MoviesList.module.css";
 
-function MoviesList(props: { movies: MovieData[]; title: string, setFavorite: Function }) {
-  const isLoading = props.movies.length === 0;
+interface MoviesListProps {
+  title: string;
+  movies: MovieData[];
+  setFavorite: (id: number, value: boolean) => void;
+}
+
+const MoviesList: FunctionComponent<MoviesListProps> = (props) => {
+  const isEmpty = props.movies.length === 0;
 
   return (
     <section className={classes.section}>
       <h1 className={classes.title}>{props.title}</h1>
-      {isLoading ? (
-        <p>Loading</p>
+      {props.children ? props.children : null}
+      {isEmpty ? (
+        <p>No results</p>
       ) : (
         props.movies.map((movie, index) => (
-          <Movie key={movie.id} movie={movie} number={index + 1} setFavorite={props.setFavorite} />
+          <Movie
+            key={movie.id}
+            movie={movie}
+            number={index + 1}
+            setFavorite={props.setFavorite}
+          />
         ))
       )}
     </section>
   );
-}
+};
 
 export default MoviesList;
